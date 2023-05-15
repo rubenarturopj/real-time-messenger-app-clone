6bc4cac5f6bce55f90de5bc79d36ebe670a94806
Ruben Perez <ruben.perezjuarez@mail.mcgill.ca>
2023-05-15 16:43:22 -0400

    fist page UI created

--- 01_instructions/02_auth.md
+++ 01_instructions/02_auth.md
@@ -10,6 +10,12 @@ We are going to use the authentication screen in our main `page.tsx`. But we are
 
 1. in the `app` folder, create a new folder called `(site)`. This is the equivalent of an `index` folder. Inside, create a new file called `page.tsx`. Copy all the content from the Main `page.tsx` and paste in in our new `page.tsx``.
 
+This will be our APP ROOT FOLDER.
+
 2. Now that you have your new `page.tsx`. Go and DELETE the main `page.tsx`.
 
 3. Run your project and it should work =)
+
+4. Style this new page with some return. Add pictures.
+
+-   You can install the Tailwind css extension
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,41 @@
-export default function Home() {
+import Image from "next/image";
+
+export default function Auth() {
     return (
-        <div>
-            <p className="text-sky-500 text-3xl">
-                Hello Index page New page.tsx in the (site) folder
-            </p>
+        <div
+            className="
+                flex 
+                min-h-full 
+                flex-col 
+                justify-center 
+                py-12 
+                sm:px-6 
+                lg:px-8 
+                bg-gray-100
+            "
+        >
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <Image
+                    className="mx-auto w-auto"
+                    src="/images/logo.png"
+                    alt="logo"
+                    width="48"
+                    height="48"
+                />
+                <h2
+                    className="
+                        mt-6 
+                        text-center 
+                        text-3xl 
+                        font-bold 
+                        tracking-tight 
+                        text-gray-900
+                    "
+                >
+                    Sign in to your account
+                </h2>
+                Here goes the Auth Forms
+            </div>
         </div>
     );
 }
